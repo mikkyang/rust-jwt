6e531d11978ccf0b4ac45e4edfbf9edfa4b8fff4
Michael Yang <mikkyangg@gmail.com>
2015-05-24 20:28:06 -0400

    Add signing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,50 @@
 extern crate crypto;
 extern crate rustc_serialize;
 
+use crypto::digest::Digest;
+use crypto::hmac::Hmac;
+use crypto::mac::Mac;
+use rustc_serialize::base64::{
+    self,
+    CharacterSet,
+    Newline,
+    ToBase64,
+};
+
 pub mod error;
 pub mod header;
 pub mod claims;
+
+const BASE_CONFIG: base64::Config = base64::Config {
+    char_set: CharacterSet::Standard,
+    newline: Newline::LF,
+    pad: false,
+    line_length: None,
+};
+
+fn sign<D: Digest>(data: &str, key: &str, digest: D) -> String {
+    let mut hmac = Hmac::new(digest, key.as_bytes());
+    hmac.input(data.as_bytes());
+
+    let mac = hmac.result();
+    let code = mac.code();
+    (*code).to_base64(BASE_CONFIG)
+}
+
+#[cfg(test)]
+mod tests {
+    use sign;
+    use crypto::sha2::Sha256;
+
+    #[test]
+    pub fn sign_data() {
+        let header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+        let claims = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+        let real_sig = "TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
+        let data = format!("{}.{}", header, claims);
+
+        let sig = sign(&*data, "secret", Sha256::new());
+
+        assert_eq!(sig, real_sig);
+    }
+}
