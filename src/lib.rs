1a60b9a67d7f0cd69ee976c52c758678b87ca8b5
Michael Yang <mikkyangg@gmail.com>
2015-05-25 09:18:48 -0400

    Make claims and header public

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,8 @@ pub mod claims;
 #[derive(Debug, Default)]
 pub struct Token {
     raw: Option<String>,
-    header: Header,
-    claims: Claims,
+    pub header: Header,
+    pub claims: Claims,
 }
 
 impl Token {
