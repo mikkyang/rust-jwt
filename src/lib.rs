fda5f694e8a6ca1f5a71117106200a326b0a8329
Michael Yang <mikkyangg@gmail.com>
2015-05-24 20:49:31 -0400

    Add verify function

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,14 @@ extern crate rustc_serialize;
 
 use crypto::digest::Digest;
 use crypto::hmac::Hmac;
-use crypto::mac::Mac;
+use crypto::mac::{
+    Mac,
+    MacResult,
+};
 use rustc_serialize::base64::{
     self,
     CharacterSet,
+    FromBase64,
     Newline,
     ToBase64,
 };
@@ -38,9 +42,23 @@ fn sign<D: Digest>(data: &str, key: &str, digest: D) -> String {
     (*code).to_base64(BASE_CONFIG)
 }
 
+fn verify<D: Digest>(target: &str, data: &str, key: &str, digest: D) -> bool {
+    let target_bytes = match target.from_base64() {
+        Ok(x) => x,
+        Err(_) => return false,
+    };
+    let target_mac = MacResult::new_from_owned(target_bytes);
+
+    let mut hmac = Hmac::new(digest, key.as_bytes());
+    hmac.input(data.as_bytes());
+
+    hmac.result() == target_mac
+}
+
 #[cfg(test)]
 mod tests {
     use sign;
+    use verify;
     use crypto::sha2::Sha256;
 
     #[test]
@@ -53,5 +71,15 @@ mod tests {
         let sig = sign(&*data, "secret", Sha256::new());
 
         assert_eq!(sig, real_sig);
+    }
+
+    #[test]
+    pub fn verify_data() {
+        let header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+        let claims = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+        let target = "TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
+        let data = format!("{}.{}", header, claims);
+
+        assert!(verify(target, &*data, "secret", Sha256::new()));
     }
 }
