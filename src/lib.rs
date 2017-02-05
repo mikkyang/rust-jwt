c44f7e58b0a731c39bc015273b32d0268fad4834
Thomas Gideon <cmdln@thecommandline.net>
2017-02-05 12:22:29 -0500

    Add RS256 support.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 extern crate crypto;
 extern crate rustc_serialize;
+extern crate openssl;
 
 use crypto::digest::Digest;
+use openssl::hash::MessageDigest;
 use rustc_serialize::{
     json,
     Decodable,
@@ -91,6 +93,19 @@ impl<H, C> Token<H, C>
         crypt::verify(sig, data, key, digest)
     }
 
+    pub fn verify_rsa(&self, key: &[u8], digest: MessageDigest) -> bool {
+        let raw = match self.raw {
+            Some(ref s) => s,
+            None => return false,
+        };
+
+        let pieces: Vec<_> = raw.rsplitn(2, '.').collect();
+        let sig = pieces[0];
+        let data = pieces[1];
+
+        crypt::verify_rsa(sig, data, key, digest)
+    }
+
     /// Generate the signed token from a key and a given hashing algorithm.
     pub fn signed<D: Digest>(&self, key: &[u8], digest: D) -> Result<String, Error> {
         let header = try!(Component::to_base64(&self.header));
@@ -98,6 +113,15 @@ impl<H, C> Token<H, C>
         let data = format!("{}.{}", header, claims);
 
         let sig = crypt::sign(&*data, key, digest);
+        Ok(format!("{}.{}", data, sig))
+    }
+
+    pub fn signed_rsa(&self, key: &[u8], digest: MessageDigest) -> Result<String, Error> {
+        let header = try!(Component::to_base64(&self.header));
+        let claims = try!(self.claims.to_base64());
+        let data = format!("{}.{}", header, claims);
+
+        let sig = crypt::sign_rsa(&*data, key, digest);
         Ok(format!("{}.{}", data, sig))
     }
 }
@@ -121,13 +145,18 @@ const BASE_CONFIG: base64::Config = base64::Config {
 mod tests {
     use crypt::{
         sign,
+        sign_rsa,
         verify,
+        verify_rsa
     };
     use Claims;
     use Token;
     use header::Algorithm::HS256;
     use header::Header;
     use crypto::sha2::Sha256;
+    use openssl::hash::MessageDigest;
+    use std::io::{Error, Read};
+    use std::fs::File;
 
     #[test]
     pub fn sign_data() {
@@ -142,6 +171,20 @@ mod tests {
     }
 
     #[test]
+    pub fn sign_data_rsa() {
+        let header = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9";
+        let claims = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+        let real_sig = "nXdpIkFQYZXZ0VlJjHmAc5/aewHCCJpT5jP1fpexUCF/9m3NxlC7uYNXAl6NKno520oh9wVT4VV/vmPeEin7BnnoIJNPcImWcUzkYpLTrDBntiF9HCuqFaniuEVzlf8dVlRJgo8QxhmUZEjyDFjPZXZxPlPV1LD6hrtItxMKZbh1qoNY3OL7Mwo+WuSRQ0mmKj+/y3weAmx/9EaTLY639uD8+o5iZxIIf85U4e55Wdp+C9FJ4RxyHpjgoG8p87IbChfleSdWcZL3NZuxjRCHVWgS1uYG0I+LqBWpWyXnJ1zk6+w4tfxOYpZFMOIyq4tY2mxJQ78Kvcu8bTO7UdI7iA";
+        let data = format!("{}.{}", header, claims);
+
+        let key = load_key("./privateKey.pem").unwrap();
+
+        let sig = sign_rsa(&*data, key.as_bytes(), MessageDigest::sha256());
+
+        assert_eq!(sig.trim(), real_sig);
+    }
+
+    #[test]
     pub fn verify_data() {
         let header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
         let claims = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
@@ -149,6 +192,17 @@ mod tests {
         let data = format!("{}.{}", header, claims);
 
         assert!(verify(target, &*data, "secret".as_bytes(), Sha256::new()));
+    }
+
+    #[test]
+    pub fn verify_data_rsa() {
+        let header = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9";
+        let claims = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+        let real_sig = "nXdpIkFQYZXZ0VlJjHmAc5/aewHCCJpT5jP1fpexUCF/9m3NxlC7uYNXAl6NKno520oh9wVT4VV/vmPeEin7BnnoIJNPcImWcUzkYpLTrDBntiF9HCuqFaniuEVzlf8dVlRJgo8QxhmUZEjyDFjPZXZxPlPV1LD6hrtItxMKZbh1qoNY3OL7Mwo+WuSRQ0mmKj+/y3weAmx/9EaTLY639uD8+o5iZxIIf85U4e55Wdp+C9FJ4RxyHpjgoG8p87IbChfleSdWcZL3NZuxjRCHVWgS1uYG0I+LqBWpWyXnJ1zk6+w4tfxOYpZFMOIyq4tY2mxJQ78Kvcu8bTO7UdI7iA";
+        let data = format!("{}.{}", header, claims);
+
+        let key = load_key("./publicKey.pub").unwrap();
+        assert!(verify_rsa(&real_sig, &*data, key.as_bytes(), MessageDigest::sha256()));
     }
 
     #[test]
@@ -171,5 +225,24 @@ mod tests {
 
         assert_eq!(token, same);
         assert!(same.verify(key, Sha256::new()));
+    }
+
+    #[test]
+    pub fn roundtrip_rsa() {
+        let token: Token<Header, Claims> = Default::default();
+        let private_key = load_key("./privateKey.pem").unwrap();
+        let raw = token.signed_rsa(private_key.as_bytes(), MessageDigest::sha256()).unwrap();
+        let same = Token::parse(&*raw).unwrap();
+
+        assert_eq!(token, same);
+        let public_key = load_key("./publicKey.pub").unwrap();
+        assert!(same.verify_rsa(public_key.as_bytes(), MessageDigest::sha256()));
+    }
+
+    fn load_key(keypath: &str) -> Result<String, Error> {
+        let mut key_file = try!(File::open(keypath));
+        let mut key = String::new();
+        try!(key_file.read_to_string(&mut key));
+        Ok(key)
     }
 }
