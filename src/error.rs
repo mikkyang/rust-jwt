6efa5cb89c5065a72ca4be1fc6635217abaf3094
Peter Wischer <peter.wischer@lwl.org>
2017-08-25 21:34:27 +0200

    impl std::error::Error for Error

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,37 @@ use std::string::FromUtf8Error;
 use base64::DecodeError;
 use serde_json::Error as JsonError;
 
+use std::error::Error as StdError;
+use std::fmt;
+
 #[derive(Debug)]
 pub enum Error {
     Format,
     Base64(DecodeError),
     Json(JsonError),
     Utf8(FromUtf8Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            Error::Format => write!(f, "Format"),
+            Error::Base64(ref x) => write!(f, "{}", x),
+            Error::Json(ref x) => write!(f, "{}", x),
+            Error::Utf8(ref x) => write!(f, "{}", x),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Format => "invalid formatting",
+            Error::Base64(ref x) => x.description(),
+            Error::Json(ref x) => x.description(),
+            Error::Utf8(ref x) => x.description(),
+        }
+    }
 }
 
 macro_rules! error_wrap {
