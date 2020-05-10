65ca2efdd35637925db0a6effa8252a5be6193be
Michael Yang <mikkyangg@gmail.com>
2020-05-10 18:48:14 +0900

    Migrate tests

--- src/claims/mod.rs
+++ src/claims/mod.rs
@@ -38,3 +38,38 @@ pub struct RegisteredClaims {
     #[serde(rename="jti", skip_serializing_if = "Option::is_none")]
     pub json_web_token_id: Option<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::default::Default;
+    use crate::claims::ClaimsV2;
+    use crate::Component;
+    use serde_json::Value;
+
+    // {"iss":"mikkyang.com","exp":1302319100,"custom_claim":true}
+    const ENCODED_PAYLOAD: &'static str = "eyJpc3MiOiJtaWtreWFuZy5jb20iLCJleHAiOjEzMDIzMTkxMDAsImN1c3RvbV9jbGFpbSI6dHJ1ZX0K";
+
+    #[test]
+    fn registered_claims() {
+        let claims = ClaimsV2::from_base64(ENCODED_PAYLOAD).unwrap();
+
+        assert_eq!(claims.registered.issuer.unwrap(), "mikkyang.com");
+        assert_eq!(claims.registered.expiration.unwrap(), 1302319100);
+    }
+
+    #[test]
+    fn private_claims() {
+        let claims = ClaimsV2::from_base64(ENCODED_PAYLOAD).unwrap();
+
+        assert_eq!(claims.private["custom_claim"], Value::Bool(true));
+    }
+
+    #[test]
+    fn roundtrip() {
+        let mut claims: ClaimsV2 = Default::default();
+        claims.registered.issuer = Some("mikkyang.com".into());
+        claims.registered.expiration = Some(1302319100);
+        let enc = claims.to_base64().unwrap();
+        assert_eq!(claims, ClaimsV2::from_base64(&*enc).unwrap());
+    }
+}
