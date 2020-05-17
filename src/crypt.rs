ed0d1122e867161f40f2df7022675dbd627da13d
Michael Yang <mikkyangg@gmail.com>
2020-05-17 17:39:36 +0900

    Replace crypt implementations

--- src/crypt.rs
+++ src/crypt.rs
@@ -1,41 +1,47 @@
-use base64;
+use crate::algorithm::rust_crypto::TypeLevelAlgorithmType;
+use crate::algorithm::{SigningAlgorithm, VerifyingAlgorithm};
+use crate::SEPARATOR;
 use crypto_mac::Mac;
 use digest::generic_array::ArrayLength;
 use digest::*;
 use hmac::Hmac;
 
+#[deprecated(
+    note = "Please use Hmac type with the SigningAlgorithm trait directly. See the source of this function for an example."
+)]
 pub fn sign<D>(data: &str, key: &[u8], _digest: D) -> String
 where
-    D: Input + BlockInput + FixedOutput + Reset + Default + Clone,
+    D: Input + BlockInput + FixedOutput + Reset + Default + Clone + TypeLevelAlgorithmType,
     D::BlockSize: ArrayLength<u8>,
     D::OutputSize: ArrayLength<u8>,
 {
     // This will panic for bad key sizes. Returning an error
     // would probably be better, but for now, I want to keep the
     // API as stable as possible
-    let mut hmac = Hmac::<D>::new_varkey(key).unwrap();
-    hmac.input(data.as_bytes());
-
-    let mac = hmac.result();
-    let code = mac.code();
-    base64::encode_config(&code, base64::URL_SAFE_NO_PAD)
+    let hmac = Hmac::<D>::new_varkey(key).unwrap();
+    let mut components = data.split(SEPARATOR);
+    let header = components.next().unwrap();
+    let claims = components.next().unwrap();
+    SigningAlgorithm::sign(&hmac, header, claims).unwrap()
 }
 
-pub fn verify<D>(target: &str, data: &str, key: &[u8], _digest: D) -> bool
+#[deprecated(
+    note = "Please use Hmac type with the VerifyingAlgorithm trait directly. See the source of this function for an example."
+)]
+pub fn verify<D>(signature: &str, data: &str, key: &[u8], _digest: D) -> bool
 where
-    D: Input + BlockInput + FixedOutput + Reset + Default + Clone,
+    D: Input + BlockInput + FixedOutput + Reset + Default + Clone + TypeLevelAlgorithmType,
     D::BlockSize: ArrayLength<u8>,
     D::OutputSize: ArrayLength<u8>,
 {
-    let target_bytes = match base64::decode_config(target, base64::URL_SAFE_NO_PAD) {
-        Ok(x) => x,
-        Err(_) => return false,
-    };
-
     // This will panic for bad key sizes. Returning an error
     // would probably be better, but for now, I want to keep the
     // API as stable as possible
-    let mut hmac = Hmac::<D>::new_varkey(key).unwrap();
-    hmac.input(data.as_bytes());
-    hmac.verify(&target_bytes).is_ok()
+    let hmac = Hmac::<D>::new_varkey(key).unwrap();
+
+    let mut components = data.split(SEPARATOR);
+    let header = components.next().unwrap();
+    let claims = components.next().unwrap();
+
+    VerifyingAlgorithm::verify(&hmac, &header, &claims, &signature).unwrap_or(false)
 }
