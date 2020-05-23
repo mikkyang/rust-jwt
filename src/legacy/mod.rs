de1bfa59ff9118b171524165bf873b11ba3fd4ed
Michael Yang <mikkyangg@gmail.com>
2020-05-23 17:49:14 +0900

    Update deprecation notices

--- src/legacy/mod.rs
+++ src/legacy/mod.rs
@@ -13,6 +13,7 @@ pub use crate::legacy::header::Header;
 pub mod claims;
 pub mod header;
 
+#[deprecated(note = "Please use jwt::Token instead")]
 #[derive(Debug, Default)]
 pub struct Token<H, C>
 where
@@ -123,6 +124,7 @@ where
     }
 }
 
+#[deprecated(note = "This is usually implemented through a blanket impl, but if needed use the ToBase64 and FromBase64 traits")]
 pub trait Component: Sized {
     fn from_base64<Input: ?Sized + AsRef<[u8]>>(raw: &Input) -> Result<Self, Error>;
     fn to_base64(&self) -> Result<String, Error>;
