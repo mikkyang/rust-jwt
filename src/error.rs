bd79e3cd3979f4b493e5f292aa360a8f66aeaf2c
Michael Yang <mikkyangg@gmail.com>
2020-05-23 15:48:50 +0900

    Add algorithm mismatch error type

--- src/error.rs
+++ src/error.rs
@@ -3,9 +3,11 @@ use crypto_mac::MacError;
 use serde_json::Error as JsonError;
 use std::fmt;
 use std::string::FromUtf8Error;
+use crate::algorithm::AlgorithmType;
 
 #[derive(Debug)]
 pub enum Error {
+    AlgorithmMismatch(AlgorithmType, AlgorithmType),
     Format,
     Base64(DecodeError),
     Json(JsonError),
@@ -18,6 +20,8 @@ pub enum Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match *self {
+            Error::AlgorithmMismatch(a, b) =>
+                write!(f, "Expected algorithm type {:?} but found {:?}", a, b),
             Error::Format => write!(f, "Format"),
             Error::Base64(ref x) => write!(f, "{}", x),
             Error::Json(ref x) => write!(f, "{}", x),
