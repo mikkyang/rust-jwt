27ac1bc07816b4dc56d1f781b4e28cd6d0f74ec0
Michael Yang <mikkyangg@gmail.com>
2020-05-23 18:36:14 +0900

    Copy readme documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,90 @@
+//! Note, for legacy support (not recommended), import from
+//! [`jwt::legacy`](legacy/index.html) instead of directly from `jwt`.
+//! Everything should work as before, with some small improvements.
+//! ### Only Claims
+//! If you don't care about that header as long as the header is verified, signing
+//! and verification can be done with just a few traits.
+//! #### Signing
+//! Claims can be any `serde::Serialize` type, usually derived with
+//! `serde_derive`.
+//! ```rust
+//! extern crate hmac;
+//! extern crate sha2;
+//!
+//! use hmac::{Hmac, Mac};
+//! use jwt::SignWithKey;
+//! use sha2::Sha256;
+//! use std::collections::BTreeMap;
+//!
+//! let key: Hmac<Sha256> = Hmac::new_varkey(b"some-secret").unwrap();
+//! let mut claims = BTreeMap::new();
+//! claims.insert("sub", "someone");
+//! let token_str = claims.sign_with_key(&key).unwrap();
+//! assert_eq!(token_str, "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzb21lb25lIn0.5wwE1sBrs-vftww_BGIuTVDeHtc1Jsjo-fiHhDwR8m0");
+//! ```
+//! #### Verification
+//! Claims can be any `serde::de::DeserializeOwned` type, usually derived with
+//! `serde_derive`.
+//! ```rust
+//! extern crate hmac;
+//! extern crate sha2;
+//!
+//! use hmac::{Hmac, Mac};
+//! use jwt::VerifyWithKey;
+//! use sha2::Sha256;
+//! use std::collections::BTreeMap;
+//!
+//! let key: Hmac<Sha256> = Hmac::new_varkey(b"some-secret").unwrap();
+//! let token_str = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzb21lb25lIn0.5wwE1sBrs-vftww_BGIuTVDeHtc1Jsjo-fiHhDwR8m0";
+//! let claims: BTreeMap<String, String> = VerifyWithKey::verify_with_key(token_str, &key).unwrap();
+//! assert_eq!(claims["sub"], "someone");
+//! ```
+//! ### Header and Claims
+//! If you need to customize the header, you can use the `Token` struct. For
+//! convenience, a `Header` struct is provided for all of the commonly defined
+//! fields, but any type that implements `JoseHeader` can be used.
+//! #### Signing
+//! Both header and claims have to implement `serde::Serialize`.
+//! ```rust
+//! extern crate hmac;
+//! extern crate sha2;
+//!
+//! use hmac::{Hmac, Mac};
+//! use jwt::{AlgorithmType, Header, SignWithKey, Token};
+//! use sha2::Sha384;
+//! use std::collections::BTreeMap;
+//!
+//! let key: Hmac<Sha384> = Hmac::new_varkey(b"some-secret").unwrap();
+//! let header = Header {
+//!     algorithm: AlgorithmType::Hs384,
+//!     ..Default::default()
+//! };
+//! let mut claims = BTreeMap::new();
+//! claims.insert("sub", "someone");
+//! let token = Token::new(header, claims).sign_with_key(&key).unwrap();
+//! assert_eq!(token.as_str(), "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJzb21lb25lIn0.WM_WnPUkHK6zm6Wz7zk1kmIxz990Te7nlDjQ3vzcye29szZ-Sj47rLNSTJNzpQd_");
+//! ```
+//! #### Verification
+//! Both header and claims have to implement `serde::de::DeserializeOwned`.
+//! ```rust
+//! extern crate hmac;
+//! extern crate sha2;
+//!
+//! use hmac::{Hmac, Mac};
+//! use jwt::{AlgorithmType, Header, Token, VerifyWithKey};
+//! use sha2::Sha384;
+//! use std::collections::BTreeMap;
+//!
+//! let key: Hmac<Sha384> = Hmac::new_varkey(b"some-secret").unwrap();
+//! let token_str = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJzb21lb25lIn0.WM_WnPUkHK6zm6Wz7zk1kmIxz990Te7nlDjQ3vzcye29szZ-Sj47rLNSTJNzpQd_";
+//! let token: Token<Header, BTreeMap<String, String>, _> = VerifyWithKey::verify_with_key(token_str, &key).unwrap();
+//! let header = token.header();
+//! let claims = token.claims();
+//! assert_eq!(header.algorithm, AlgorithmType::Hs384);
+//! assert_eq!(claims["sub"], "someone");
+//! ```
+
+
 extern crate base64;
 extern crate crypto_mac;
 extern crate digest;
