97a405600fb97eba4532ca318e255c709ae01a2f
Michael Yang <mikkyangg@gmail.com>
2020-05-30 15:40:59 +0900

    Implement Store for all Index types that take in strings

--- src/algorithm/store.rs
+++ src/algorithm/store.rs
@@ -1,5 +1,19 @@
+use std::ops::Index;
+
+/// A store of keys that can be retrieved by key id.
 pub trait Store {
     type Algorithm: ?Sized;
 
     fn get(&self, key_id: &str) -> Option<&Self::Algorithm>;
+}
+
+impl<T, A> Store for T
+where
+    for<'a> T: Index<&'a str, Output = A>,
+{
+    type Algorithm = A;
+
+    fn get(&self, key_id: &str) -> Option<&A> {
+        Some(&self[key_id])
+    }
 }
