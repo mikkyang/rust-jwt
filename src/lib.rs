cd74901aedc77ab9e10adb94b73b740d2dc92e9e
Michael Yang <mikkyangg@gmail.com>
2020-05-30 18:40:19 +0900

    Remove unwrap

--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,10 @@
 //!
 //! # use jwt::Error;
 //! # fn try_main() -> Result<(), Error> {
-//! let key: Hmac<Sha256> = Hmac::new_varkey(b"some-secret").unwrap();
+//! let key: Hmac<Sha256> = Hmac::new_varkey(b"some-secret")?;
 //! let mut claims = BTreeMap::new();
 //! claims.insert("sub", "someone");
-//! let token_str = claims.sign_with_key(&key).unwrap();
+//! let token_str = claims.sign_with_key(&key)?;
 //! assert_eq!(token_str, "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzb21lb25lIn0.5wwE1sBrs-vftww_BGIuTVDeHtc1Jsjo-fiHhDwR8m0");
 //! # Ok(())
 //! # }
