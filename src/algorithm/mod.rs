61c399cb88859f3263a3f40aa6f780303e793cff
Michael Yang <mikkyangg@gmail.com>
2020-05-30 14:52:42 +0900

    Add AsRef blanket implementations

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -65,3 +65,24 @@ pub trait VerifyingAlgorithm {
         self.verify_bytes(header, claims, &*signature_bytes)
     }
 }
+
+// TODO: investigate if these AsRef impls are necessary
+impl<T: AsRef<dyn VerifyingAlgorithm>> VerifyingAlgorithm for T {
+    fn algorithm_type(&self) -> AlgorithmType {
+        self.as_ref().algorithm_type()
+    }
+
+    fn verify_bytes(&self, header: &str, claims: &str, signature: &[u8]) -> Result<bool, Error> {
+        self.as_ref().verify_bytes(header, claims, signature)
+    }
+}
+
+impl<T: AsRef<dyn SigningAlgorithm>> SigningAlgorithm for T {
+    fn algorithm_type(&self) -> AlgorithmType {
+        self.as_ref().algorithm_type()
+    }
+
+    fn sign(&self, header: &str, claims: &str) -> Result<String, Error> {
+        self.as_ref().sign(header, claims)
+    }
+}
