1fca92669192c2469595f0041353ec7ed4b196fb
Michael Yang <mikkyangg@gmail.com>
2020-05-30 18:26:02 +0900

    Just use regular error type

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 use crate::algorithm::AlgorithmType;
 use base64::DecodeError;
-use crypto_mac::MacError;
+use crypto_mac::{InvalidKeyLength, MacError};
 use serde_json::Error as JsonError;
 use std::fmt;
 use std::string::FromUtf8Error;
@@ -15,6 +15,7 @@ pub enum Error {
     Json(JsonError),
     Utf8(FromUtf8Error),
     RustCryptoMac(MacError),
+    RustCryptoMacKeyLength(InvalidKeyLength),
     #[cfg(feature = "openssl")]
     OpenSsl(openssl::error::ErrorStack),
 }
@@ -32,6 +33,7 @@ impl fmt::Display for Error {
             Error::Json(ref x) => write!(f, "{}", x),
             Error::Utf8(ref x) => write!(f, "{}", x),
             Error::RustCryptoMac(ref x) => write!(f, "{}", x),
+            Error::RustCryptoMacKeyLength(ref x) => write!(f, "{}", x),
             #[cfg(feature = "openssl")]
             Error::OpenSsl(ref x) => write!(f, "{}", x),
         }
@@ -52,31 +54,11 @@ error_wrap!(DecodeError, Error::Base64);
 error_wrap!(JsonError, Error::Json);
 error_wrap!(FromUtf8Error, Error::Utf8);
 error_wrap!(MacError, Error::RustCryptoMac);
+error_wrap!(InvalidKeyLength, Error::RustCryptoMacKeyLength);
 #[cfg(feature = "openssl")]
 error_wrap!(openssl::error::ErrorStack, Error::OpenSsl);
 
 #[cfg(test)]
 pub(crate) mod tests {
-    #[derive(Debug)]
-    pub enum TestError {
-        LibError(super::Error),
-        KeyError(crypto_mac::InvalidKeyLength),
-    }
-
-    impl<T> From<T> for TestError
-    where
-        T: Into<super::Error>,
-    {
-        fn from(error: T) -> Self {
-            TestError::LibError(error.into())
-        }
-    }
-
-    impl From<crypto_mac::InvalidKeyLength> for TestError {
-        fn from(error: crypto_mac::InvalidKeyLength) -> Self {
-            TestError::KeyError(error)
-        }
-    }
-
-    pub type TestResult = Result<(), TestError>;
+    pub type TestResult = Result<(), super::Error>;
 }
