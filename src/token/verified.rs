d505fa55463eb59ce1fb3e7870fbce07af3c50de
Michael Yang <mikkyangg@gmail.com>
2021-01-17 16:01:17 -0800

    Replace generic Index implementation with specific implementations for BTreeMap and HashMap

--- src/token/verified.rs
+++ src/token/verified.rs
@@ -146,7 +146,7 @@ pub(crate) fn split_components(token: &str) -> Result<[&str; 3], Error> {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::BTreeMap;
+    use std::collections::{BTreeMap, HashMap};
 
     use hmac::{Hmac, NewMac};
     use serde::Deserialize;
@@ -225,8 +225,24 @@ mod tests {
     }
 
     #[test]
-    pub fn verify_claims_with_store() -> Result<(), Error> {
+    pub fn verify_claims_with_b_tree_map() -> Result<(), Error> {
         let mut key_store = BTreeMap::new();
+        let key1: Hmac<Sha256> = Hmac::new_varkey(b"first")?;
+        let key2: Hmac<Sha512> = Hmac::new_varkey(b"second")?;
+        key_store.insert("first_key", Box::new(key1) as Box<dyn VerifyingAlgorithm>);
+        key_store.insert("second_key", Box::new(key2) as Box<dyn VerifyingAlgorithm>);
+
+        let claims: Claims =
+        "eyJhbGciOiJIUzUxMiIsImtpZCI6InNlY29uZF9rZXkifQ.eyJuYW1lIjoiSmFuZSBEb2UifQ.t2ON5s8DDb2hefBIWAe0jaEcp-T7b2Wevmj0kKJ8BFxKNQURHpdh4IA-wbmBmqtiCnqTGoRdqK45hhW0AOtz0A"
+            .verify_with_store(&key_store)?;
+
+        assert_eq!(claims.name, "Jane Doe");
+        Ok(())
+    }
+
+    #[test]
+    pub fn verify_claims_with_hash_map() -> Result<(), Error> {
+        let mut key_store = HashMap::new();
         let key1: Hmac<Sha256> = Hmac::new_varkey(b"first")?;
         let key2: Hmac<Sha512> = Hmac::new_varkey(b"second")?;
         key_store.insert("first_key", Box::new(key1) as Box<dyn VerifyingAlgorithm>);
